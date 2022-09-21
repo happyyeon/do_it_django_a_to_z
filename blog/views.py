61f2b50cbd8ac33c24760abd0374ae52dee12d87
happyyeon <88064555+happyyeon@users.noreply.github.com>
2022-09-22 04:00:18 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
-from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Category, Tag
 
 
@@ -29,6 +30,20 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
 
         return context
+
+# 포스트 생성 페이지, LoginRequiredMixin --> Login 했을 때만 출력
+class PostCreate(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','hook_text','content','head_image','file_upload','category']
+
+    # form_valid 재정의
+    def form_valid(self,form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate,self).form_valid(form)
+        else:
+            return redirect('/blog/')
 
 def category_page(request,slug):
     if slug == 'no_category':
