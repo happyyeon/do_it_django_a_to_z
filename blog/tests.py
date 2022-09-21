466957ee82b2ba99f04f5903fed4e1e8fd3d1103
happyyeon <88064555+happyyeon@users.noreply.github.com>
2022-09-22 01:34:48 +0900

    포스트 목록 페이지와 포스트 상세 페이지 태그 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase,Client
 from bs4 import BeautifulSoup
-from .models import Post,Category
+from .models import Post,Category,Tag
 
 # Create your tests here.
 class TestView(TestCase):
@@ -11,8 +11,14 @@ class TestView(TestCase):
         self.user_trump = User.objects.create_user(username='trump',password='somepassword')
         self.user_obama = User.objects.create_user(username='obama',password='somepassword')
 
+        # 가상 카테고리 생성
         self.category_programming = Category.objects.create(name='programming',slug='programming')
         self.category_music = Category.objects.create(name='music', slug='music')
+
+        # 가상 태그 생성
+        self.tag_python_kor = Tag.objects.create(name='파이썬 공부',slug='파이썬-공부')
+        self.tag_python = Tag.objects.create(name='python',slug='python')
+        self.tag_hello = Tag.objects.create(name='hello', slug='hello')
 
         self.post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
@@ -20,6 +26,9 @@ class TestView(TestCase):
             category=self.category_programming,
             author=self.user_trump
         )
+        # Tag Add
+        self.post_001.tags.add(self.tag_hello)
+
         self.post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
             content='1등이 전부는 아니잖아요?',
@@ -32,6 +41,9 @@ class TestView(TestCase):
             content='카테고리가 없을 수도 있죠 뭐 ㅋ',
             author=self.user_obama
         )
+
+        self.post_003.tags.add(self.tag_python_kor)
+        self.post_003.tags.add(self.tag_python)
 
 
     # Navigation Bar 잘 작동되는지 확인
@@ -77,17 +89,30 @@ class TestView(TestCase):
         post_001_card = main_area.find('div', id='post-1')
         self.assertIn(self.post_001.title,post_001_card.text)
         self.assertIn(self.post_001.category.name, post_001_card.text)
+        self.assertIn(self.post_001.author.username.upper(),post_001_card.text)
+        self.assertIn(self.tag_hello.name,post_001_card.text)
+        self.assertNotIn(self.tag_python_kor.name, post_001_card.text)
+        self.assertNotIn(self.tag_python.name, post_001_card.text)
 
         post_002_card = main_area.find('div', id='post-2')
         self.assertIn(self.post_002.title,post_002_card.text)
         self.assertIn(self.post_002.category.name,post_002_card.text)
+        self.assertIn(self.post_002.author.username.upper(), post_002_card.text)
+        self.assertNotIn(self.tag_hello.name, post_002_card.text)
+        self.assertNotIn(self.tag_python_kor.name, post_002_card.text)
+        self.assertNotIn(self.tag_python.name, post_002_card.text)
 
         post_003_card = main_area.find('div',id='post-3')
         self.assertIn('미분류',post_003_card.text)
         self.assertIn(self.post_003.title,post_003_card.text)
+        self.assertIn(self.post_003.author.username.upper(), post_003_card.text)
+        self.assertNotIn(self.tag_hello.name, post_003_card.text)
+        self.assertIn(self.tag_python_kor.name, post_003_card.text)
+        self.assertIn(self.tag_python.name, post_003_card.text)
 
         self.assertIn(self.user_trump.username.upper(), main_area.text)
         self.assertIn(self.user_obama.username.upper(), main_area.text)
+
 
         # 포스트가 없는 경우
         Post.objects.all().delete()
@@ -127,6 +152,12 @@ class TestView(TestCase):
         self.assertIn(self.user_trump.username.upper(), main_area.text)
         #2.6 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
         self.assertIn(self.post_001.content,post_area.text)
+
+        # 2.7 첫 번째 포스트의 태그가 포스트 영역에 있다.
+        self.assertIn(self.tag_hello.name,post_area.text)
+        self.assertNotIn(self.tag_python.name, post_area.text)
+        self.assertNotIn(self.tag_python_kor.name, post_area.text)
+
 
     # 카테고리 페이지 테스트 하기
     def test_category_page(self):
