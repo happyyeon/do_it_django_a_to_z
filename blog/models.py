1fb25c617a73034bdceade1bd3daa211e0f46124
happyyeon <88064555+happyyeon@users.noreply.github.com>
2022-09-24 04:10:15 +0900

    대문 페이지 수정하기

--- blog/models.py
+++ blog/models.py
@@ -61,6 +61,12 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1294/177331d29d6f0c20/svg/{self.author.email}'
+
 # 댓글 모델
 class Comment(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
